a5ddd4c6a71f864199d1a5cadadcecba3f099f47
Iskren Donev <iskrend@zettahost.com>
2017-04-30 15:25:50 +0300

    The calendar now displays after 1.5 seconds
    Also, I have moved the included script files in the <head> HTML
    container.

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+setTimeout(function(){ $(document).ready(function () {
     $('#calendar').fullCalendar({
         dayClick: function () {
             alert('a day has been clicked!');
         }
     })
 
-});
+}); }, 1500);
