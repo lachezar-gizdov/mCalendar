8f24fcc18b388c64d9c15d86259191f880359281
Lucho Gizdov <olebg01@gmail.com>
2017-05-08 16:10:05 +0300

    add templates engine and homeController

---
+++ public/js/templates.js
@@ -0,0 +1,26 @@
+let templates = function () {
+    var handlebars = window.handlebars || window.Handlebars,
+        Handlebars = window.handlebars || window.Handlebars,
+        cache = {};
+
+    function get(name) {
+        let promise = new Promise(function (resolve, reject) {
+            if (cache[name]) {
+                resolve(cache[name]);
+                return;
+            }
+            
+            let url = `templates/${name}.handlebars`;
+            $.get(url, function (html) {
+                let template = handlebars.compile(html);
+                cache[name] = template;
+                resolve(template);
+            });
+        });
+        return promise;
+    }
+
+    return {
+        get: get
+    };
+}();
